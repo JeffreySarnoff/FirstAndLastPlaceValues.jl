4e8787d6b3041655f6780c56fc101f2bd78bc0bb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-12-12 03:58:08 -0500

    Update unit_first_place.jl

--- src/unit_first_place.jl
+++ src/unit_first_place.jl
@@ -1,4 +1,21 @@
+two(::Type{T}) where {T<:Number} = one{T} + one{T}
 
+fracbits(::Type{T}) where {T<:Real} = precision(T) - 1
+
+implicitbit(::Type{Float16})  = one(UInt16)  << precision(Float16)
+implicitbit(::Type{Float32})  = one(UInt32)  << precision(Float32)
+implicitbit(::Type{Float64})  = one(UInt64)  << precision(Float64)
+implicitbit(::Type{Float128}) = one(UInt128) << precision(Float128)
+
+significant_bits(::Type{T}) where {T} = precision(T)
+significant_mask(::Type{T}) where {T} = Base.significand_mask(T) | implicitbit(T)
+
+# twopow_eplicit(T) = 2^fracbits(T)  
+# 2^(count of explicitly stored significand bits)
+# 2^trailing_significand_bits
+twopow_explicit(::Type{T}) where {T<:Real} = trunc(Int64, two(T)^fracbits(T))
+
+function trailing_ones(Base.significand_mask(T)) where {T<:Real}
 
 #=
   Float16  => 2^Base.significand_bits(Float16)
